42a40cd63c11a744064faf64020cdfe995aa3435
bigbl <adamkostandy@gmail.com>
2022-02-22 13:11:50 -0500

    Stack Implementation

--- Stack.cpp
+++ Stack.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "Stack.h"
+
+
+Stack::Car::Car() {
+    timesMoved = 0;
+    name = "";
+}
+
+
+void Stack::Prepend(Car* newCar) {
+    Car* temp = new Car();
+        head->next = temp;
+
+    }
+
+void Stack::loadFile() {
+    ifstream inFile;
+    string line;
+    inFile.open("C:\\Users\\bigbl\\CLionProjects\\autoComplete\\assests\\scratch.txt"); //Absolute path for now cause im bad.
+
+    /* Check if file is open, then we check if head is nullptr, if its not nullptr then do normal functionality */
+    if(inFile.is_open()) {
+        while (std::getline(inFile, line)) {
+            Car* temp = new Car();
+            if(head == nullptr) {
+                head = temp;
+                head->name = line;
+                stackSize++;
+            } else {
+                Prepend(temp);
+                stackSize++;
+            }
+        }
+        inFile.close();
+    } else {
+        cout << "File input is not working!";
+    }
+}
--- Stack.h
+++ Stack.h
@@ -2,13 +2,42 @@
 // Created by bigbl on 2/22/2022.
 //
 
+
 #ifndef GARAGEPROJ_STACK_H
 #define GARAGEPROJ_STACK_H
 
+#include "string"
+#include "iostream"
+#include "fstream"
+
+using namespace std;
 
 class Stack {
 
-};
 
+public:
+    struct Car {
+        string name;
+        int timesMoved{};
+        Car *next = nullptr;
+        Car *prev = nullptr;
+
+        Car();
+
+    };
+
+
+    Car *head = nullptr;
+    int stackSize = 10;
+
+    /**
+     * Functions for loading files into nodes.
+     */
+
+    void loadFile();
+    void Prepend(Car* newCar);
+
+
+};
 
 #endif //GARAGEPROJ_STACK_H
